f02e12c4701a46881b060e48778e893ff1925166
robson felipe <robsonfelipeprog@gmail.com>
2025-04-25 16:10:55 -0400

    feat : adicionando o nrrock

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "http://127.0.0.1:3000/api",
+    baseURL: "https://8447-131-161-126-34.ngrok-free.app",
     withCredentials: true
 })
