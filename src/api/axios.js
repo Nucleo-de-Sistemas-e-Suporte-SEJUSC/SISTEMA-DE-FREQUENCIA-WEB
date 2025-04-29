b22949f9aa909c8e37642586cc1be6952c702348
robson felipe <robsonfelipeprog@gmail.com>
2025-04-29 11:36:11 -0400

    fix : 03

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "https://cfc9-131-161-126-34.ngrok-free.app",
+    baseURL: "https://c850-131-161-126-34.ngrok-free.app ",
     //baseURL: "http://127.0.0.1:3000/api",
     withCredentials: true
 })
