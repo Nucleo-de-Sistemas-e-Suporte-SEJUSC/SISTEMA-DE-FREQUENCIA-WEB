12ce45a06964401e330ecb7c28eb6aa48030dd78
lucasnather <natherlucas@gmail.com>
2025-05-05 15:49:58 -0400

    fix : rota de login pra 8000

--- src/components/formularios/form-login/index.jsx
+++ src/components/formularios/form-login/index.jsx
@@ -15,7 +15,7 @@ export function FormLogin() {
             event.preventDefault();
 
             try {
-                const response = await fetch('http://127.0.0.1:3000/login', {
+                const response = await fetch('http://127.0.0.1:8000/login', {
                     method: "POST",
                     headers: {
                         "Content-Type": "application/json"
