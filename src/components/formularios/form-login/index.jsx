ab05fb6a51f055acf75b90ef972b4c685ac113da
Nucleo-de-Sistemas-e-Suporte-SEJUSC <nss@sejusc.am.gov.br>
2025-05-19 15:09:46 -0400

    feat : adicionando os arquivos necessarios para o refresh da pagina

--- src/components/formularios/form-login/index.jsx
+++ src/components/formularios/form-login/index.jsx
@@ -15,7 +15,7 @@ export function FormLogin() {
             event.preventDefault();
 
             try {
-                const response = await fetch('http://12.90.4.191:8000/login', {
+                const response = await fetch('http://12.90.4.98:8000/login', {
                     method: "POST",
                     headers: {
                         "Content-Type": "application/json"
